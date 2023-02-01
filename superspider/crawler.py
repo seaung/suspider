7a00370de921a793c84fed3530134bcf2daceb33
seaung <seaung.c0m7de@protonmail.com>
2023-02-01 08:26:43 +0800

    commit coder

--- superspider/crawler.py
+++ superspider/crawler.py
@@ -1,13 +1,57 @@
-from superspider.pipline import get_md5
+import os
+import sys
+import sqlite3
+
+from sqlite3 import Connection
+
+from PyQt5.QtWidgets import QApplication
+
+from superspider.pipline import get_md5, duplicate
 from superspider.downloader import Downloader
-from superspider.urls import Dom
 
 
 class Crawler(object):
     def __init__(self, url: str) -> None:
-        self.allow_urls = [url]
+        self.urls = [url]
         self.checker_urls = []
         self.id = [0]
+        self.conn = self._connect_db(url=url)
+
+    def _normalize(self, url: str) -> str:
+        url = url.rstrip("/") + "/"
+        if url.find(":") != -1:
+            url = url[0:url.find(":")+1].lower() + "//" + url[url.find(":") + 1:].lstrip("/")
+        return url
+
+    def _is_empty(self) -> bool:
+        return True if not self.urls else False
+
+    def _connect_db(self, url: str) -> Connection:
+        db_file = get_md5(url)
+        if os.path.exists(f"./db/{db_file}.db3"):
+            os.remove(f"./db/{db_file}.db3")
+
+        conn = sqlite3.connect(f"./db/{db_file}.db3", check_same_thread=False)
+        conn.execute("")
+        return conn
+
+    def crawler(self) -> None:
+        app = QApplication(sys.argv)
+        while not self._is_empty():
+            for item in self.urls:
+                print(f"current item : {item}")
+
+            downloader = Downloader(app=app, checker_urls=self.checker_urls)
+            downloader.run(self.urls)
+            app.exec_()
+            self.urls = []
+            for item in self.checker_urls:
+                if not duplicate(self.conn, self.id, item):
+                    self.urls.append(item)
+
+            self.urls = []
 
     def start(self) -> None:
-        pass
+        self.crawler()
+        self.conn.close()
+
--- superspider/downloader.py
+++ superspider/downloader.py
@@ -1,9 +1,14 @@
-from functools import partial
-from typing import Any, List
+#from functools import partial
+from typing import Any, Dict, List
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtCore import QUrl
+from PyQt5.QtWebEngineWidgets import QWebEngineView, QWebEngineSettings
+
+from superspider.urls import Dom
 
 
 class Downloader(object):
-    def __init__(self, app: Any, checker_urls: List[Any] = []) -> None:
+    def __init__(self, app: QApplication, checker_urls: List[Any] = []) -> None:
         self.app = app
         self.url = ""
         self.urls_result = checker_urls
@@ -12,11 +17,32 @@ class Downloader(object):
     def _get_path(self, url: str) -> str:
         return url[0:url.rfind("/")]
 
-    def _loader_finished(self, browser_id) -> None:
+    def _loader_finished(self, browser_id: Dict[QWebEngineView, bool]) -> None:
         web_view, _ = self.browser[browser_id]
         self.browser[browser_id] = (web_view, True)
+        frame = web_view.page()
+        dom = frame.toHtml()
+        self.urls_result += parse_dom(dom, self.url)
+        web_view.loadFinished(False)
+        web_view.stop()
+
+        if all([closed for _, closed in self.browser.values()]):
+            self.app.quit()
 
     def run(self, urls: List[str]) -> List[Any | str]:
         for browser_id, url in enumerate(urls):
             self.url = self._get_path(url)
-        return []
+            web_view = QWebEngineView()
+            web_view.settings().setAttribute(QWebEngineSettings.WebAttribute.AutoLoadImages, False)
+            #loader = partial(self._loader_finished, browser_id)
+            web_view.loadFinished(True)
+            web_view.load(QUrl(url=url))
+            self.browser[browser_id] = (web_view, False)
+
+        return self.urls_result
+
+
+def parse_dom(content: Any, url: str) -> List[str]:
+    dom = Dom(content, url)
+    return dom.get_url()
+
